5df8b92987e9ae7160e48a89a46e68d45ebc943c
kachakaran6 <kachakaran06@gmail.com>
2024-09-29 02:11:04 +0530

    push this code

---
+++ binary_search.c
@@ -0,0 +1,26 @@
+// Binary Search in C
+#include <stdio.h>
+int binarySearch(int array[], int x, int low, int high) {
+// Repeat until the pointers low and high meet each other
+while (low <= high) {
+int mid = low + (high - low) / 2;
+if (array[mid] == x)
+return mid;
+if (array[mid] < x)
+low = mid + 1;
+else
+high = mid - 1;
+}
+return -1;
+}
+int main(void) { 
+int array[] = {3, 4, 5, 6, 7, 8, 9};
+int n = 7;
+int x = 9;
+int result = binarySearch(array, x, 0, n - 1);
+if (result == -1)
+printf("Not found");
+else
+printf("Element is found at index %d", result);
+return 0;
+}
